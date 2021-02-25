203a9b85aac416117927501b6ca189db16da5f21
MichaelWoo-git <mw47@njit.edu>
2021-02-25 16:14:51 -0500

    completed aprior algorithm with sets

--- Aprior_3.0.py
+++ Aprior_3.0.py
@@ -12,7 +12,7 @@ print(support_percent)
 confidence_percent = input("Please enter the percentage of Confidence you want?\n")
 print(confidence_percent)
 
-
+# These are my dictionaries to choose which store to get based in Key-Value Pairs
 def number_to_store(store_number):
     switcher = {
         1: "data/amazon_transactions.csv",
@@ -22,7 +22,6 @@ def number_to_store(store_number):
         5: "data/walmart_transaction.csv"
     }
     return switcher.get(store_number)
-
 
 def number_to_item_list_of_store(store_number):
     switcher_dict = {
@@ -34,21 +33,6 @@ def number_to_item_list_of_store(store_number):
     }
     return switcher_dict.get(store_number)
 
-
-# def calculate_support(transaction, counts_of_items, min_support_percent):
-#     # this will be the denominator
-#     transaction_size = len(transaction)
-#     # print(type(transaction_size))
-#     # print(counts_of_items)
-#     name_items = np.array(counts_of_items["item_name"])
-#     support_calculation = (counts_of_items["number_of_count"] / transaction_size) * 100
-#     df = pd.DataFrame({"item_name": name_items, "support": support_calculation})
-#     df_meet_min_support = pd.DataFrame(df.loc[df["support"] >= min_support_percent])
-#     print(df_meet_min_support)
-#     # check to see if item is in set
-#     return df_meet_min_support
-
-
 def a_priori_read(item_list, transaction, support_percentage, confidence_percentage):
     # Create two different functions one that is solo for read in the file data and the other that is algorithmic with the data
     if support_percentage > 100 or confidence_percentage > 100 or support_percentage < 0 or confidence_percentage < 0:
@@ -56,7 +40,6 @@ def a_priori_read(item_list, transaction, support_percentage, confidence_percent
         print("Restarting Apriori 2.0.....\n")
         time.sleep(2)
         os.system("python APrior_2.0.py")
-
     if support_percentage >= 0 and support_percentage <= 100 and confidence_percentage >= 0 and confidence_percentage <= 100:
         df2 = pd.read_csv(item_list)
         df1 = pd.read_csv(transaction)
@@ -64,79 +47,72 @@ def a_priori_read(item_list, transaction, support_percentage, confidence_percent
         print(df2.head())
         trans = np.array(df1["transaction"])
         items_names = np.array(df2["item_name"])
-        counter = np.zeros(len(items_names), dtype=int)
-        print(counter)
+        k_value =1
+        ap(items_names, trans, support_percentage, confidence_percentage,k_value)
+
+def ap(items_names, trans, support_percentage, confidence_percentage,k_value):
+
+    counter = np.zeros(len(items_names), dtype=int)
+    #print(counter)
+    if k_value == 1:
         for i in trans:
             i = list((map(str.strip, i.split(','))))
             s1 = set(i)
-            print(s1)
-            print(s1)
+            # print(s1)
+            # print(s1)
             nums = 0
             for x in items_names:
                 s2 = set()
                 s2.add(x)
-                print(i)
-                print(s2)
-                print(s2.issubset(s1))
+                # print(i)
+                # print(s2)
+                # print(s2.issubset(s1))
                 if s2.issubset(s1):
                     counter[nums] += 1
-                    print(counter)
+                #print(counter)
                 nums += 1
-        print(counter)
-        # result = map(lambda x: x + x, numbers)
-        counter = list(map(lambda x: int((x/20)*100),counter))
-        df3 = pd.DataFrame({"item_name": items_names,"freq":counter})
-        print(df3)
-        rslt_df = df3[df3['freq'] > support_percentage]
-        print(rslt_df)
-        exit(12321)
-        # item_name = np.array(df2["item_name"])
-        # trans = list(map(str.strip,trans))
-        # item_name = list(map(str.strip,item_name))
-        # print(list(map( lambda s: s.replace(",",""), trans )))
-        # exit(12321)
-        # counter = np.zeros(len(item_name), dtype=int)
-        # print(trans)
-        # for i in trans:
-        #     i = i.split(",")
-        #     print(set(i))
-        k_val = 1
-        # a_priori_do(item_name, trans, support_percentage, confidence_percentage, k_val)
+    if k_value > 1:
+        for i in trans:
+            i = list((map(str.strip, i.split(','))))
+            s1 = set(i)
+            #print(s1)
+            nums = 0
+            for x in items_names:
+                s2 = set()
+                x = np.asarray(x)
+               # print(x)
+                for q in x:
+                 #   print(q)
+                    s2.add(q)
+                #print(i)
+                #print(s2)
+                #print(s2.issubset(s1))
+                if s2.issubset(s1):
+                    counter[nums] += 1
+                   # print(counter)
+                nums += 1
 
+    #print(counter)
+    counter = list(map(lambda x: int((x / len(trans)) * 100), counter))
+    df3 = pd.DataFrame({"item_name": items_names, "freq": counter})
+    #print(df3)
+    rslt_df = df3[df3['freq'] > support_percentage]
 
-#
-# def a_priori_do(item_name, trans, support_percentage, confidence_percentage, k_val):
-#     counter = np.zeros(len(item_name), dtype=int)
-#     # When K =1
-#     for row in trans:
-#         row = row.split(',')
-#         # this will be our indexer for our counter
-#         i = 0
-#         # K = 1
-#         if k_val == 1:
-#             for item in item_name:
-#                 if item in row:
-#                     counter[i] = counter[i] + 1
-#                 # onto the next item
-#                 i += 1
-#     df3 = pd.DataFrame({"item_name": item_name, "number_of_count": counter})
-#     print(df3)
-#     print(counter)
-#     items_that_meet_support = calculate_support(trans, df3, support_percentage)
-#     k_value = 1
-#     if len(items_that_meet_support["item_name"]) == 1:
-#         print("The most frequent item in the transaction is: " + str(items_that_meet_support.at[0, "item_name"]))
-#     if len(items_that_meet_support["item_name"]) > 1:
-#         k_val += 1
-#         items = np.array(items_that_meet_support["item_name"])
-#         comb = combinations(items, k_val)
-#         comb = list(comb)
-#         df_items = pd.DataFrame({"item_name": comb})
-#         df_items = list(df_items["item_name"])
-#         print(df_items)
-#         k_val += 1
-#         a_priori_do(df_items, trans, support_percentage, confidence_percentage, k_val)
-
+    print(rslt_df)
+    if not rslt_df.empty:
+        print(k_value)
+        k_value+=1
+        df_items = pd.read_csv(str(number_to_item_list_of_store(int(store_num))))
+        names_of_items = np.array(df_items["item_name"])
+        print(names_of_items)
+        item_combs = np.array(df_items["item_name"])
+        comb = combinations(item_combs, k_value)
+        comb = list(comb)
+        # print(len(comb))
+        # for t in comb:
+        #     for r in t:
+        #         print(r)
+        ap(comb,trans,support_percentage,confidence_percentage,k_value)
 
 a_priori_read(str(number_to_item_list_of_store(int(store_num))), str(number_to_store(int(store_num))),
               int(support_percent), int(confidence_percent))
