d20bba63933198605c736562a737d1498a8eaffa
unknown <michaelwoo120@gmail.com>
2021-03-01 13:59:03 -0500

    updated files

--- Aprior_3.0.py
+++ Aprior_3.0.py
@@ -4,6 +4,7 @@ import os
 import time
 from itertools import permutations, combinations
 
+# The begining of the algorithm prompts
 print("Welcome to Apriori 2.0!")
 store_num = input("Please select your store \n 1. Amazon \n 2. Nike \n 3. Best Buy \n 4. K-Mart \n 5. Walmart\n")
 print(store_num)
@@ -23,6 +24,7 @@ def number_to_store(store_number):
     }
     return switcher.get(store_number)
 
+
 def number_to_item_list_of_store(store_number):
     switcher_dict = {
         1: "data/amazon_item_names.csv",
@@ -32,6 +34,7 @@ def number_to_item_list_of_store(store_number):
         5: "data/walmart_item_names.csv"
     }
     return switcher_dict.get(store_number)
+
 
 def a_priori_read(item_list, transaction, support_percentage, confidence_percentage):
     # Create two different functions one that is solo for read in the file data and the other that is algorithmic with the data
@@ -47,13 +50,13 @@ def a_priori_read(item_list, transaction, support_percentage, confidence_percent
         print(df2.head())
         trans = np.array(df1["transaction"])
         items_names = np.array(df2["item_name"])
-        k_value =1
-        ap(items_names, trans, support_percentage, confidence_percentage,k_value)
+        k_value = 1
+        ap(items_names, trans, support_percentage, confidence_percentage, k_value)
 
-def ap(items_names, trans, support_percentage, confidence_percentage,k_value):
 
+def ap(items_names, trans, support_percentage, confidence_percentage, k_value):
     counter = np.zeros(len(items_names), dtype=int)
-    #print(counter)
+    # print(counter)
     if k_value == 1:
         for i in trans:
             i = list((map(str.strip, i.split(','))))
@@ -69,42 +72,42 @@ def ap(items_names, trans, support_percentage, confidence_percentage,k_value):
                 # print(s2.issubset(s1))
                 if s2.issubset(s1):
                     counter[nums] += 1
-                #print(counter)
+                # print(counter)
                 nums += 1
     if k_value > 1:
         for i in trans:
             i = list((map(str.strip, i.split(','))))
             s1 = set(i)
-            #print(s1)
+            # print(s1)
             nums = 0
             for x in items_names:
                 s2 = set()
                 x = np.asarray(x)
-               # print(x)
+                # print(x)
                 for q in x:
-                 #   print(q)
+                    #   print(q)
                     s2.add(q)
-                #print(i)
-                #print(s2)
-                #print(s2.issubset(s1))
+                # print(i)
+                # print(s2)
+                # print(s2.issubset(s1))
                 if s2.issubset(s1):
                     counter[nums] += 1
-                   # print(counter)
+                # print(counter)
                 nums += 1
 
-    #print(counter)
+    # print(counter)
     counter = list(map(lambda x: int((x / len(trans)) * 100), counter))
     df3 = pd.DataFrame({"item_name": items_names, "freq": counter})
-    #print(df3)
+    # print(df3)
     rslt_df = df3[df3['freq'] > support_percentage]
 
     print(rslt_df)
     if not rslt_df.empty:
         print(k_value)
-        k_value+=1
+        k_value += 1
         df_items = pd.read_csv(str(number_to_item_list_of_store(int(store_num))))
         names_of_items = np.array(df_items["item_name"])
-        print(names_of_items)
+       # print(names_of_items)
         item_combs = np.array(df_items["item_name"])
         comb = combinations(item_combs, k_value)
         comb = list(comb)
@@ -112,7 +115,9 @@ def ap(items_names, trans, support_percentage, confidence_percentage,k_value):
         # for t in comb:
         #     for r in t:
         #         print(r)
-        ap(comb,trans,support_percentage,confidence_percentage,k_value)
+        ### create permutations here
+        ap(comb, trans, support_percentage, confidence_percentage, k_value)
+
 
 a_priori_read(str(number_to_item_list_of_store(int(store_num))), str(number_to_store(int(store_num))),
               int(support_percent), int(confidence_percent))
